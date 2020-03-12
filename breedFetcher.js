189387ac1914d1ffa0dff13a043e171ecf717599
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-12 12:26:06 -0400

    handles api and query errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,20 @@
 const request = require('request');
 
-if (process.argv.length!==3){
+if (process.argv.length !== 3) {
   return console.error('Usage: node breedFetcher.js breedName');
 }
 
 const query = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
+  if (error) {
+    return console.error('Error accessing API!');
+  }
+
   const data = JSON.parse(body);
-  //console.log(data);
-  console.log(typeof data);
+  if (!body.description) {
+    return console.error('Invalid query!');
+  }
+
   console.log(data[0].description);
 });
